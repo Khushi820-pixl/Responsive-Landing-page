76105dc97ce07a82b979e44654a4495ccc9e549d
Khushi820-pixl <dautpurekhushi@gmail.com>
2026-08-30 22:15:13 +0530

    Add scroll event listener to change navbar style

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbar = document.getElementById("navbar");
+
+// Change navigation style when scrolling
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
